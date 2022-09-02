3d6ae7ba8f88e651a12d87d28376f48495869898
James Prince <james.prince1@gmail.com>
2022-09-02 14:14:37 +1000

    added pseudo code to the quiz function. Wrote scores init function framework.

---
+++ assets/script/quiz.js
@@ -0,0 +1,36 @@
+var startButton = document.querySelector('#start-button');
+
+var results = {
+    scores: []
+}
+
+
+function startQuiz () {
+    //clear existing content from display card
+    //select questions from database/collection
+
+
+    // start timer
+    //for each question:
+    //print question text
+    //generate button list with data-value 1-4 
+    //event listener on list listening for button presses
+    //if button clicked, stop event listener and compare data-value to answer index
+
+    //if correct, display correct and move to next question
+    //if incorrect, deduct time and next question 
+    
+    //once 5 questions have been answered, give score and ask for name/initials
+
+    //save initials and score in local storage
+
+}
+
+
+
+
+
+
+
+
+startButton.addEventListener('click', startQuiz)
